2f3f1e643cb7318fbfcbb0bb976cc2e55f8a4290
auxghlann <orenoytemail@gmail.com>
2025-02-06 23:34:06 +0800

    feat: create OOP-based structure instead of all codes in main

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         // declare var to be used
         int states;
         String[] alpha;
         String[] init_states, fin_states;
-        int[][] matrix;
+//        int[][] matrix;
         String input;
 
         // initialize classes to be used
         Scanner in = new Scanner(System.in);
-        DFA dfa_obj = new DFA();
-
 
         // >> Begin of Program
         System.out.print("Enter the number of states: ");
@@ -27,24 +25,18 @@ public class Main {
             System.out.println(a);
         }
         // Create matrix using states (row) and len of alpha (col)
-        matrix = new int[states][alpha.length];
-
-        System.out.printf("Row: %d \nCol: %d\n", matrix.length, matrix[0].length);
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[1].length; j++) {
-                System.out.printf("Transition %d(%s): ", i, alpha[j]);
-                matrix[i][j] = in.nextInt();
-            }
-        }
+//        matrix = new int[states][alpha.length];
 
         System.out.print("Enter the initial state/s (sep by comma): ");
         init_states = in.next().split(",");
         System.out.print("Enter the final state/s (sep by comma): ");
         fin_states = in.next().split(",");
 
+        DFA dfa_obj = new DFA(states, alpha, init_states, fin_states);
 
-        dfa_obj.display_matrix(matrix, alpha, init_states, fin_states);
+        dfa_obj.fill_matrix();
+
+        dfa_obj.display_matrix();
 
         boolean loopState = true;
 
@@ -52,7 +44,7 @@ public class Main {
             System.out.print("Enter the input string: ");
             input = in.next();
 
-            boolean is_accepted = dfa_obj.validate_string(input, alpha, matrix, fin_states);
+            boolean is_accepted = dfa_obj.validate_string(input);
 
             if (is_accepted) {
                 System.out.println("Remarks: Accepted");
@@ -68,7 +60,7 @@ public class Main {
             }
         }
 
-        System.out.println("GGs");
+        System.out.println("Program Finished!");
 
         // End of Program <<
 
--- src/dfa/DFA.java
+++ src/dfa/DFA.java
@@ -1,47 +1,58 @@
 package dfa;
+import java.util.Scanner;
 
 public class DFA {
 
-    public DFA() {
+    protected final int states;
+    protected final String[] input_alpha;
+    protected final String[] init_states;
+    protected final String[] fin_states;
+    protected int[][] matrix;
+//    protected String input;
 
+    public DFA(int states, String[] input_alpha, String[] init_states, String[] fin_states) {
+        this.states = states;
+        this.input_alpha = input_alpha;
+        this.init_states = init_states;
+        this.fin_states = fin_states;
     }
 
-    public boolean check_if_initial_state(String state, String[] init_states) {
+    protected boolean check_if_initial_state(String state) {
 
-        for (String a : init_states) {
+        for (String a : this.init_states) {
             if (state.equals(a)) return true;
         }
         return false;
     }
 
-    public boolean check_if_final_state(String state, String[] fin_states) {
+    protected boolean check_if_final_state(String state) {
 
-        for (String a : fin_states) {
+        for (String a : this.fin_states) {
             if (state.equals(a)) return true;
         }
         return false;
     }
 
-    public boolean validate_string(String input, String[] alpha, int[][] matrix, String[] fin_states) {
+    public boolean validate_string(String input) {
 
         // zero at default because we always start at the init state
         int row_pointer = 0;
 
         // Convert input to numerical
-        int[] converted_in = convert_input_to_numeric(input, alpha);
+        int[] converted_in = convert_input_to_numeric(input);
 
-        for (int i : converted_in) {
-            row_pointer = matrix[row_pointer][i];
+        for (int i = 0; i < converted_in.length; i++) {
+            row_pointer = this.matrix[row_pointer][converted_in[i]];
         }
 
-        return check_if_final_state(String.valueOf(row_pointer), fin_states);
+        return check_if_final_state(String.valueOf(row_pointer));
     }
 
     // Helper function for validate_string
 
-    public int[] convert_input_to_numeric(String input, String[] alpha) {
+    protected int[] convert_input_to_numeric(String input) {
 
-        String converted_alpha = String.join("", alpha);
+        String converted_alpha = String.join("", this.input_alpha);
         int[] converted_input = new int[input.length()];
         for (int i = 0; i < input.length(); i++) {
 
@@ -55,32 +66,54 @@ public class DFA {
 //        return false;
 //    }
 
-    public void display_matrix(int[][] matrix, String[] alpha, String[] init_states, String[] fin_states) {
+    public void fill_matrix() {
+
+        this.matrix = new int[states][input_alpha.length];
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Row: %d \nCol: %d\n", this.matrix.length, this.matrix[0].length);
+
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix[1].length; j++) {
+                System.out.printf("Transition (" +  i + "," + this.input_alpha[j] +"): ");
+                this.matrix[i][j] = in.nextInt();
+            }
+        }
+    }
+
+    public void display_matrix() throws Exception {
+
+        if (matrix == null) {
+            throw new NullPointerException("Matrix should not be empty!");
+        }
+
         System.out.print("states ");
-        for (String a: alpha) {
-            System.out.printf(" %s  ", a);
+        for (int i = 0; i < this.input_alpha.length; i++) {
+            String a = this.input_alpha[i];
+            System.out.print(" " + a + "  ");
         }
         System.out.println();
 
+
         int counter = 0;
-        for (int[] integers : matrix) {
+        for (int i = 0; i < this.matrix.length; i++) {
+            int[] integers = this.matrix[i];
+
             // Check for initial and final states for displaying
-            if (check_if_initial_state(String.valueOf(counter), init_states)) {
-                System.out.printf(" -q%d  |", counter);
-            }
-            else if (check_if_final_state(String.valueOf(counter), fin_states)) {
-                System.out.printf(" +q%d  |", counter);
-            }
-            else {
-                System.out.printf("  q%d  |", counter);
+            if (check_if_initial_state(String.valueOf(counter))) {
+                System.out.print(" -q" + counter + "  |");
+            } else if (check_if_final_state(String.valueOf(counter))) {
+                System.out.print(" +q" + counter + "  |");
+            } else {
+                System.out.print("  q" + counter + "  |");
             }
 
-            for (int j = 0; j < matrix[1].length; j++) {
-                System.out.printf(" %s |", integers[j]);
+            for (int j = 0; j < this.matrix[0].length; j++) { // Use matrix[0].length for consistency
+                System.out.print(" " + integers[j] + " |");
             }
             System.out.println();
             counter++;
         }
+
     }
 
 }
